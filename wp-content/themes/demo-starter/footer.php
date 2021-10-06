29f52977a576bb68c5aace69324f15542f1aff54
gilang aditya <madegilangaditya32@gmail.com>
2021-10-06 14:03:24 +0800

    add dumny content

---
+++ wp-content/themes/demo-starter/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Demo_Starter_Theme
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demo-starter' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'demo-starter' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'demo-starter' ), 'demo-starter', '<a href="http://mdgilangaditya.com">Gilang Aditya</a>' );
+				?>
+		</div><!-- .site-info -->
+		<div class="custom-footer">
+			<?php esc_html_e( "Custom Footer text", 'demo-starter' ); ?>  
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
