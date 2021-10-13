fce77bf008fcabfed36f0f75427523b2c6ba148f
gilang aditya <madegilangaditya32@gmail.com>
2021-10-13 09:48:58 +0800

    add markup and sidebar in index.php

--- wp-content/themes/wphierarchy/footer.php
+++ wp-content/themes/wphierarchy/footer.php
@@ -1,6 +1,14 @@
     <?php wp_footer(); ?>
     
         </div> <!-- #content close -->
+
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <a href="<?php echo esc_url( __( 'https://wordpress.org' , 'wphierarchy' ) ); ?>">
+                <?php
+                    printf( esc_html__( 'Proudly Powered by %s', 'wphierarchy' ), 'Wordpress' );
+                ?>
+            </a>
+        </footer>
     </div> <!-- .Page close -->
 </body>
 </html>
