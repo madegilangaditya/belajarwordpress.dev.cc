c2ad49c2bdfaeab2b842a6302116e02247cee81d
gilang aditya <madegilangaditya32@gmail.com>
2021-10-15 12:39:30 +0800

    add template for category archive by id and by slug

--- wp-content/themes/wphierarchy/footer-splash.php
+++ wp-content/themes/wphierarchy/footer-splash.php
@@ -1,8 +1,15 @@
-<div class="notice">
-        New - Footer splash
-    </div>
-
 <?php wp_footer(); ?>
+    
+    </div> <!-- #content close -->
 
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <a href="<?php echo esc_url( __( 'https://wordpress.org' , 'wphierarchy' ) ); ?>">
+            <?php
+                printf( esc_html__( 'Proudly Powered by %s New - Footer splash', 'wphierarchy' ), 'Wordpress' );
+            ?>
+        </a>
+
+    </footer>
+</div> <!-- .Page close -->
 </body>
 </html>
--- wp-content/themes/wphierarchy/header-splash.php
+++ wp-content/themes/wphierarchy/header-splash.php
@@ -2,9 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
 </head>
-<body>
-    <div class="notice">
-        New - Header splash
-    </div>
+<body <?php body_class(); ?>>
+    <div class="page">
+
+        <a href="#content" class="skip-link screen-reader-text">
+            <?php esc_html_e( 'Skip to content', 'wphierarchy' ); ?>
+        </a>
+
+        <header id="masthead" class="site-header" role="banner">
+
+            <div class="site-branding">
+                <p class="site-title">
+                    <a href="<?php esc_url( home_url( '/' ) ); ?>" rel="home">
+                         New - Header splash
+                    </a>
+                </p>
+
+                <p class="site-description">
+                    Blog Description
+                </p>
+            </div>
+
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <?php
+                    $args = [
+                        'theme_location' => 'main-menu'
+                    ];
+                    wp_nav_menu( $args );
+                    ?>
+            </nav>
+
+        </header>
+
+        <div id="content" class="site-content">
+
+        
+
+                    
