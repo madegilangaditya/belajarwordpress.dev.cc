68fad3714cc26f2fc0170708e849909bfe113dcb
gilang aditya <madegilangaditya32@gmail.com>
2021-10-15 11:40:51 +0800

    create author.php that showing author description and author profile picture

---
+++ wp-content/themes/wphierarchy/author.php
@@ -0,0 +1,41 @@
+<?php get_header(  ); ?>
+    <style>
+        .author-img img{
+            float:none;
+        }
+    </style>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <div class="author-bio">
+                <h1><?php the_archive_title( '' ); ?></h1>
+                <p>
+                    <?php echo the_author_meta( 'description', $post->post_author ); ?>
+                </p>
+                <div class="author-img">
+                    
+                    <?php echo get_avatar(get_the_author_meta('ID')); ?>
+
+                </div> </br>
+            </div>
+
+        <!-- Post Loop -->
+        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+            <?php get_template_part( 'template-parts/content-posts', get_post_format(  ) ); ?>
+            
+
+        <?php endwhile; else :  ?>
+
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php endif; ?>
+
+        <?php echo paginate_links( ); ?>
+        </main>
+        <p>Template: Archive.php</p>
+    </div>
+
+    <?php get_sidebar(); ?>
+
+   
+
+<?php get_footer(  ); ?>
