f3cf273d872703ee6feda0c4f62a5100a09ba24f
gilang aditya <madegilangaditya32@gmail.com>
2021-10-15 11:02:55 +0800

    add home.php with include links in blog post title, pagination in blog page, create custom content post for home.php which set as blog page

---
+++ wp-content/themes/wphierarchy/template-parts/content-posts-gallery.php
@@ -0,0 +1,25 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+
+        <span class="dashicons dashicons-format-<?php echo get_post_format( $post->ID ); ?>"></span>
+        <p><?php esc_html_e( 'Enjoy this gallery post', 'wphierarchy' ); ?></p>
+
+        <?php the_title( '<h2><a href=" '. esc_url( get_permalink() ) .' ">', '</a></h2>' ); ?>
+
+        <div class="byline">
+            <?php esc_html_e( 'Author:', 'wphierarchy' ); ?>  <?php the_author(); ?>
+        </div>
+    </header>
+
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div>
+
+    <!-- Don't show comments at all -->
+    <?php if( comments_open() ): ?>
+
+        <?php comments_template(); ?>
+
+    <?php endif; ?>
+
+</article>
