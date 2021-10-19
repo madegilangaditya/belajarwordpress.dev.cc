c436dfab8c12a505864d29ea5b906c5b6dc79991
gilang aditya <madegilangaditya32@gmail.com>
2021-10-19 13:19:48 +0800

    add 404.php and add some changes in content-none.php

--- wp-content/themes/wphierarchy/template-parts/content-none.php
+++ wp-content/themes/wphierarchy/template-parts/content-none.php
@@ -1,6 +1,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h1><?php esc_html_e( 'Sorry no content found', 'wphierarchy' ); ?></h1>
+        <h1><?php esc_html_e( '404 - Page not found', 'wphierarchy' ); ?></h1>
     </header>
+
+    <div class="entry-content">
+        <p><?php esc_html_e( 'Sorry No content Found', 'wphierarchy' ); ?></p>
+        <p><?php echo get_search_form(); ?></p>
+    </div>
 
 </article>
