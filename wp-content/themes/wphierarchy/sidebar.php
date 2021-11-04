b411d9e7afd88282aeddc5c4ede4aa6775a93c51
gilang aditya <madegilangaditya32@gmail.com>
2021-11-04 10:08:53 +0800

    play around with wp_get_archives() tags

--- wp-content/themes/wphierarchy/sidebar.php
+++ wp-content/themes/wphierarchy/sidebar.php
@@ -6,6 +6,18 @@
 
 <div id="secondary" class="widget-area" role="complementary">
 
+    <?php 
+        $args = [
+            'type' => 'yearly',
+            'limit' => 3,
+            'show_post_count' => true,
+            'order' => 'DESC'
+        ];
+        
+        wp_get_archives( $args );
+    
+    ?>
+
     <?php dynamic_sidebar( 'main-sidebar' ); ?>
 
 </div>
