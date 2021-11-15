7a7e2ec2dededd0212481aed6257d485cbf8efa3
gilang aditya <madegilangaditya32@gmail.com>
2021-11-15 17:01:25 +0800

    add copyright on footer and wp login form on sidebar

--- wp-content/themes/wphierarchy/footer.php
+++ wp-content/themes/wphierarchy/footer.php
@@ -3,6 +3,8 @@
         </div> <!-- #content close -->
 
         <footer id="colophon" class="site-footer" role="contentinfo">
+
+            &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>
             <a href="<?php echo esc_url( __( 'https://wordpress.org' , 'wphierarchy' ) ); ?>">
                 <?php
                     printf( esc_html__( 'Proudly Powered by %s', 'wphierarchy' ), 'Wordpress' );
--- wp-content/themes/wphierarchy/sidebar.php
+++ wp-content/themes/wphierarchy/sidebar.php
@@ -6,6 +6,22 @@
 
 <div id="secondary" class="widget-area" role="complementary">
 
+    <?php if( !is_user_logged_in(  ) ): ?>
+
+        <?php wp_login_form( ); ?>
+
+    <?php else: ?>
+        <p>
+            <a href="<?php echo wp_logout_url( get_the_permalink() ); ?>" class="button">
+                <?php _e( 'Logout','wphierarchy' ); ?>
+            </a>
+        </p>
+    
+    <?php endif; ?>
+
+    <h3><?php _e('Calendar', 'wphierarchy'); ?></h3>
+    <?php get_calendar( ); ?>
+
     <?php 
         $args = [
             'type' => 'yearly',
