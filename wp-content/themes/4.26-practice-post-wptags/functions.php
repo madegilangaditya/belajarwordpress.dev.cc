800a6813116f337a6280accd949267e656e64dcb
gilang aditya <madegilangaditya32@gmail.com>
2021-11-26 14:44:34 +0800

    add custom hook for admin page when post save as draft

--- wp-content/themes/4.26-practice-post-wptags/functions.php
+++ wp-content/themes/4.26-practice-post-wptags/functions.php
@@ -139,4 +139,17 @@ function wphooks_add_draft_to_title( $post_id ){
 }
 add_action( 'save_post', 'wphooks_add_draft_to_title' );
 
+// Practice 1 Hook - Load Admin stylesheet when edit draft post
+function wphooks_draft_mode_styles(){
+  global $post;
+
+  if( !$post ) return;
+
+  if( 'draft' === $post->post_status ){
+    wp_enqueue_style( 'wphooks-admin-css', get_stylesheet_directory_uri() . 'assests/css/admin.css', [], time(), 'all' );
+    add_editor_style( 'assets/css/visual-editor.css' );
+  }
+}
+add_action( 'admin_enqueue_scripts', 'wphooks_draft_mode_styles' );
+
 ?>
