929e0ae6e19d3d89873c2a9c402e3f2436b3c65e
gilang aditya <madegilangaditya32@gmail.com>
2021-12-17 14:44:09 +0800

    add option array as settings

--- wp-content/plugins/gilang-plugin/includes/wpplugin-options.php
+++ wp-content/plugins/gilang-plugin/includes/wpplugin-options.php
@@ -1,14 +1,37 @@
 <?php
 
-// Function to learning how to add options
-// SQL Query : SELECT * FROM wp_options WHERE option_name = "gilang_plugin_option";
+// // Function to learning how to add options
+// // SQL Query : SELECT * FROM wp_options WHERE option_name = "gilang_plugin_option";
+// function gilang_plugin_options(){
+
+//     if( !get_option( 'gilang_plugin_option' ) ){
+//         add_option( 'gilang_plugin_option', 'My plugins options' );
+//     }
+    
+//     update_option( 'gilang_plugin_option', 'My Updated plugins options' );
+//     //delete_option( 'gilang_plugin_option' );
+
+// }
+// add_action( 'admin_init', 'gilang_plugin_options' );
+
+// Function to learning how to add options multiple value
 function gilang_plugin_options(){
+    // $options = [
+    //     'First Name',
+    //     'Second option',
+    //     'Third Option'
+    // ];
+
+    $options = [];
+    $options['name'] = 'Gilang';
+    $options['location'] = 'Washington DC';
+    $options['sponsor'] = 'Plugin.CO';   
 
     if( !get_option( 'gilang_plugin_option' ) ){
-        add_option( 'gilang_plugin_option', 'My plugins options' );
+        add_option( 'gilang_plugin_option', $options );
     }
     
-    update_option( 'gilang_plugin_option', 'My Updated plugins options' );
+    update_option( 'gilang_plugin_option', $options );
     //delete_option( 'gilang_plugin_option' );
 
 }
--- wp-content/plugins/gilang-plugin/templates/admin/settings-page.php
+++ wp-content/plugins/gilang-plugin/templates/admin/settings-page.php
@@ -1,5 +1,17 @@
 <div class="wrap">
         <h1><?php esc_html_e( get_admin_page_title(  ) ); ?></h1>
-        <p><?php esc_html_e( 'Options: ', 'gilang' ); ?></p>
-        <p><?php esc_html_e( get_option( 'gilang_plugin_option' ) ); ?></p>
+
+        <h2><?php esc_html_e( 'All Options', 'gilang' ); ?></h2>
+        <?php $options = get_option( 'gilang_plugin_option' ); ?>
+
+        <ul>
+            <?php foreach($options as $option): ?>
+                <li><?php echo $option; ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+        <?php if(array_key_exists('name', $options)): ?>
+            <h2><?php esc_html_e( 'Specific Option', 'gilang' ); ?></h2>
+            <p><?php esc_html_e( $options['name'] ); ?></p>
+        <?php endif; ?>
     </div>
